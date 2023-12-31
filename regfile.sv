d0027d9fc433f41a1dfcaebea71c9fd35627bef3
Richard Sun <richardsun.gz@gmail.com>
2023-12-31 00:08:59 -0800

    new regfile should be done

--- regfile.sv
+++ regfile.sv
@@ -1,5 +1,48 @@
-module regfile(input [15:0] w_data, input [2:0] w_addr, input w_en, input [2:0] r_addr, input clk, output [15:0] r_data);
-  reg [15:0] m[0:7];
-  assign r_data = m[r_addr];
-  always_ff @(posedge clk) if (w_en) m[w_addr] <= w_data;
+module regfile(input [31:0] w_data, input [5:0] w_addr, input w_en, input [5:0] r_addr, input clk, output [31:0] r_data);
+
+    /*
+    *** About ***
+    - 17 regsiteres 32 bits each
+    - 4 bits for address
+    - 32 bits for data
+    - read is combinational
+    - write is sequential
+
+    *** Registers ***
+    R0 - General Purpose
+    R1 - General Purpose
+    R2 - General Purpose
+    R3 - General Purpose
+    R4 - General Purpose
+    R5 - General Purpose
+    R6 - General Purpose
+    R7 - Holds System Call Number
+    R8 - General Purpose
+    R9 - General Purpose
+    R10 - General Purpose
+    R11 - Frame Pointer (FP)
+    R12 - Intra Procedural Call (IP)
+    R13 - Stack Pointer (SP)
+    R14 - Link Register (LR)
+    R15 - Program Counter (PC)
+    R16 - Status Register (SR)
+    */
+
+    reg [31:0] regsiteres[0:17];
+
+    // read is combinational
+    always_comb begin
+        if (r_addr < 5'd17) begin
+            r_data = regsiteres[r_addr];
+        end else begin
+            r_data = 0;
+        end
+    end
+
+    // write is sequential
+    always_ff @(posedge clk) begin
+        if (w_en && w_addr < 5'd17) begin
+            regsiteres[w_addr] = w_data;
+        end
+    end
 endmodule: regfile
