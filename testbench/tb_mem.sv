5bd494c657b6248798019c636e8c503ea9494e27
Richard Sun <richardsun.gz@gmail.com>
2024-01-11 01:10:53 -0800

    changed mem tb

--- testbench/tb_mem.sv
+++ testbench/tb_mem.sv
@@ -1,29 +1,34 @@
-module tb_mem()
+`timescale 1 ps / 1 ps
+module tb_mem();
 
 // Inputs
 reg clk;
-reg [10:0]  addr;
-reg [31:0]  data_in;
-reg w_en;
+reg [10:0]  addr_a, addr_b;
+reg [31:0]  data_a, data_b;
+reg en_a, en_b;
 
 // Outputs
-wire [31:0]  data_out;
+wire [31:0]  data_out_a, data_out_b;
 
 // DUT
-intru_mem mem (
-    .addr(addr),
-    .clk(clk),
-    .data_in(data_in),
-    .w_en(w_en),
-    .data_out(data_out)
+duel_mem mem (
+    .address_a(addr_a),
+    .address_b(addr_b),
+    .clock(clk),
+    .data_a(data_a),
+    .data_b(data_b),
+    .wren_a(en_a),
+    .wren_b(en_b),
+    .q_a(data_out_a),
+    .q_b(data_out_b)
 );
 
 task clkR;
     begin
         #5;
-        clk = 1'b0;
-        #5;
         clk = 1'b1;
+        #5;
+        clk = 1'b0;
     end
 endtask: clkR
 
@@ -31,16 +36,34 @@ initial begin
     //initial clkR
     clkR;
 
-    //write value 1 to memory address 0
-    addr = 0;
-    w_en = 1'b1;
-    data_in = 32'h00000001;
+    //write value 1 to memory address 0 && val 2 to mem addr 1
+    addr_a = 0;
+    en_a = 1'b1;
+    data_a = 32'h00000001;
+    addr_b = 1;
+    en_b = 1'b1;
+    data_b = 32'h00000002;
     clkR;
-    w_en = 1'b0;
+    en_a = 1'b0;
+    en_b = 1'b0;
 
-    //read value from memory address 0
-    #1;
-    addr = 0;
-    
+    //start reading from memory address 3 so reads should be 0
+    addr_a = 3;
+    clkR;
+    clkR;
+    clkR;
+    clkR;
+    clkR;
+
+    //start reading from memory address 0 so reads should be 1 then addr 1 so reads should be 2 while printing out the values
+    addr_a = 0;
+    clkR;
+    addr_a = 1;
+    $display("data_out_a = %d\n", data_out_a);
+    clkR;
+    $display("data_out_a = %d\n", data_out_b);
+    clkR;
+    $display("data_out_a = %d\n", data_out_a); 
+
 end
 endmodule: tb_mem
